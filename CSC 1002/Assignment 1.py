49e3c4f91633387ef3f5e0e332c4908a03800105
arsity <120090645@link.cuhk.edu.cn>
2021-03-17 14:15:12 +0800

    dev

--- CSC 1002/Assignment 1.py
+++ CSC 1002/Assignment 1.py
@@ -1,3 +1,6 @@
+import copy  # dependencies
+
+
 def generating(n, total_List):  # generating the initial table in order
     for row in range(0, n):  # generate the full table
         list_temp = list(range(row*n+1, row*n+n+1))
@@ -6,21 +9,6 @@ def generating(n, total_List):  # generating the initial table in order
     global place
     place = [n-1, n-1]  # to state the location of the space
     return total_List
-
-
-def mess(a):
-    import random
-    for i in range(0, 10000):
-        op = random.choice(['up', 'down', 'right', 'left'])
-        a = operation(op, a)
-    return a
-
-
-def display(a):  # display the table
-    for row in a:
-        for column in row:
-            print("%+3s" % (column), end=" ")  # to display on certain position
-        print("\n", end="")
 
 
 def operation(op, l):
@@ -43,22 +31,39 @@ def operation(op, l):
     return l
 
 
+def mess(a):  # to make a random table at start
+    import random
+    for i in range(0, 10000):
+        op = random.choice(['up', 'down', 'right', 'left'])
+        a = operation(op, a)
+    return a
+
+
+def display(a):  # display the table
+    for row in a:
+        for column in row:
+            print("%+3s" % (column), end=" ")  # to display on certain position
+        print("\n", end="")
+
+
 count = 0  # define the count to calculate steps
 n = 3  # determine the size of the table
 place = [0, 0]  # to initialize location of the space
 total_List = []  # to initialize the table
-total_List = generating(n, total_List)  # generating the initial list
-initial_List = total_List
-total_List = mess(total_List)
-display(total_List)
+total_List = generating(n, total_List)  # generating the initial table
+initial_List = copy.deepcopy(total_List)  # save the original table
+total_List = mess(total_List)  # mess the table at start
+display(total_List)  # show the table
 while True:
-    list_For_Judge = total_List
-    total_List = operation(input('Select your operation: '), total_List)
-    display(total_List)
-    count += 1
+    # to judge if invalid operation in followings
+    list_For_Judge = copy.deepcopy(total_List)
+    total_List = operation(
+        input('Select your operation: '), total_List)  # operation of the table
+    display(total_List)  # show the result
+    count += 1  # count the steps
     if list_For_Judge == total_List:
-        count += -1
+        count += -1  # if valid, cancel the counting step
         print("Invalid Operation!")
-    if total_List == initial_List:
+    if total_List == initial_List:  # to judge if finish
         print("Congratulations!")
         break
