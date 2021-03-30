2d988f64f655d612ca0566354001394159dfe1b8
arsity <qweertryr@outlook.com>
2021-03-31 00:30:42 +0800

    master

--- CSC 1001/Assignment 2/Q4.py
+++ CSC 1001/Assignment 2/Q4.py
@@ -12,11 +12,26 @@ def input_Test():
             return word
 
 
-word1 = input_Test()
-word2 = input_Test()
-wordList1 = list(word1.lower()).sort()
-wordList2 = list(word2.lower()).sort()
-if wordList1 == wordList2:
-    print(word1, 'and', word2, 'is an anagram.')
-else:
-    print(word1, 'and', word2, 'is not an anagram.')
+def transform(word1, word2):
+    wordList1 = list(word1.lower())
+    wordList2 = list(word2.lower())
+    wordList1.sort()
+    wordList2.sort()
+    return wordList1, wordList2
+
+
+def result(wordList1, wordList2, word1, word2):
+    if wordList1 == wordList2:
+        print(word1, 'and', word2, 'is an anagram.')
+    else:
+        print(word1, 'and', word2, 'is not an anagram.')
+
+
+def main():
+    word1 = input_Test()
+    word2 = input_Test()
+    a, b = transform(word1, word2)
+    result(a, b, word1, word2)
+
+
+main()
