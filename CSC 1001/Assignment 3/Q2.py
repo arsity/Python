5e6e53df60fd62d799d05a7fea78e45fa28b20a7
arsity <qweertryr@outlook.com>
2021-04-12 18:12:22 +0800

    dev

--- CSC 1001/Assignment 3/Q2.py
+++ CSC 1001/Assignment 3/Q2.py
@@ -1,4 +1,14 @@
+
+
 class polynomial:
-    def __init__(self, polynomial):
-        self.polynomial = polynomial
-    
+    def __init__(self, polynomial='0'):
+        polynomialList = polynomial.split('+')
+        polynomialList = tuple(
+            filter(lambda x: not x.isnumeric(), polynomialList))
+
+
+    def __str__(self):
+        return self.answer
+
+
+print(polynomial(input('Please enter your polynomial in standard algebraic notation:\n')))
