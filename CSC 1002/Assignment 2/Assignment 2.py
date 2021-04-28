71c4ce39c8d2468715147d941d7e7b2bd83c70d0
arsity <qweertryr@outlook.com>
2021-04-28 23:50:30 +0800

    dev

--- CSC 1002/Assignment 2/Assignment 2.py
+++ CSC 1002/Assignment 2/Assignment 2.py
@@ -3,22 +3,24 @@ import random
 
 
 def initialization():
+
     turtle.setup(660, 740)
+    turtle.tracer(False, 300)
     frame_Turtle = turtle.Turtle(visible=False)
-    turtle.tracer(False)
     frame_Turtle.penup()
     frame_Turtle.goto(-250, 210)
     frame_Turtle.pendown()
-    frame_Turtle.goto(-250, -290)
-    frame_Turtle.goto(250, -290)
-    frame_Turtle.goto(250, 210)
-    frame_Turtle.goto(-250, 210)
-    frame_Turtle.goto(-250, 290)
-    frame_Turtle.goto(250, 290)
-    frame_Turtle.goto(250, 210)
+
+    for _ in range(4):
+        frame_Turtle.forward(500)
+        frame_Turtle.right(90)
+
+    for _ in range(2):
+        frame_Turtle.forward(500)
+        frame_Turtle.left(90)
+        frame_Turtle.forward(80)
+        frame_Turtle.left(90)
     turtle.update()
-    turtle.Screen()
-    turtle.screensize(500, 500)
 
     welcomeText_Turtle = turtle.Turtle(visible=False)
     welcomeText_Turtle.penup()
@@ -27,13 +29,38 @@ def initialization():
                              align='left', font=('Arial', 12, 'normal'))
 
 
+def placeFruit():
+    pass
+
+
+def draw(head: turtle, locationList: list) -> list:
+    stampId_List = []
+    head.color('blue', 'black')
+    for location in locationList:
+        head.goto(location)
+        stamp_Id = head.stamp()
+        stampId_List.append(stamp_Id)
+    head.color('red')
+    return stampId_List
+
+
+def statusBar(contact: int, time: float, motion: str):
+    status_Turtle = turtle.Turtle(visible=False)
+    status_Turtle.penup()
+    status_Turtle.goto(0, 250)
+    status_Turtle.write("", align='center', font=('Arial', 14, 'normal'))
+
+
+def move(motion: str):
+    
+
+def game():
+    pass
+
+
 def main():
     initialization()
-    snake = turtle.Turtle('square')
-    snake.color('red')
-    snake.penup()
-
-    snake.forward(100)
+    turtle.ontimer(game(), 300)
     turtle.mainloop()
 
 
